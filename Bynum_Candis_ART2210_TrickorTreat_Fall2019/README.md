cb20629c57288a5f279b96e3874c0e227fd98f2a
Candis Bynum <55206246+cbynum3@users.noreply.github.com>
2019-10-20 20:22:57 -0500

    Add files via upload

---
+++ Bynum_Candis_ART2210_TrickorTreat_Fall2019/README.md
@@ -0,0 +1,5 @@
+# Creative Coding Game
+
+Have fun with my Halloween Trivia Game!
+
+[Trick Or Treat](https://cbynum3.github.io/Bynum_Candis_ART2210_TrickorTreat_Fall2019/index.html)
---
+++ Bynum_Candis_ART2210_TrickorTreat_Fall2019/sketch.js
@@ -0,0 +1,17 @@
+function setup() {
+  createCanvas(700,700);
+}
+
+var myQuestions = [{
+    question: "What is given out to kids when they go Trick or Treating?",
+    answers: {
+        a: 'tricks'
+        b: 'candy'
+        c: 'pumpkins'
+    },
+    correctAnswer: 'b'
+},
+{
+    question: 'What movie is played everyday during the month of October?',
+    answers
+}
